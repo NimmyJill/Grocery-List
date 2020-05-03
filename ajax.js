4c09e6d831b2da8c1880b0c611e5eee55e10a9ff
NimmyJill <62232412+NimmyJill@users.noreply.github.com>
2020-05-03 12:52:28 +0530

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,35 @@
+function ajax(){
+    document.getElementById("dbutn").style.visibility="hidden";
+    // Creating an XHR Object
+    var xhttp= new XMLHttpRequest();
+    // EventListener
+    xhttp.onreadystatechange= function(){
+        // condition
+        if(this.readyState==4 && this.status==200)  {
+           
+            $(document).ready(function(){
+                $.getJSON("items.json", function(data){
+                var output="";
+                $.each(data, function(key, value){
+                
+                    output += '<tr><td>' + value.SlNo + '</td>';
+                    output += '<td>' + value.name+ '</td>';                    
+                    output += '<td> <img src=' + value.image + ' width="80" height="80"></img> </td>';
+                    output += '<td>' + value.Quantity + '</td>';
+                    output +='<td>' + value.Unit + '</td>';
+                    output +='<td>' + value.Department+ '</td>';
+                    output += '<td>' + value.Notes+ '</td></tr>';                  
+                   
+                });
+                
+                $('#item_list').append(output);
+                
+                 });
+                });
+        }
+    }
+    
+    xhttp.open("GET", "items.json", true);
+    xhttp.send();
+    }
+    
